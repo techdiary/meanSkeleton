38cfae9400414431092c8cb556f83ff091bfd3dc
Aayush Sharma <greatash90@gmail.com>
2017-10-13 20:50:15 +0530

    Update api.js

--- server/routes/api.js
+++ server/routes/api.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const router = express.Router()
 var DB = require('../db');
-var uri = "mongodb://aayush24:sharmaji@meanbeta-shard-00-00-sshqm.mongodb.net:27017,meanbeta-shard-00-01-sshqm.mongodb.net:27017,meanbeta-shard-00-02-sshqm.mongodb.net:27017/users?ssl=true&replicaSet=MEANbeta-shard-0&authSource=admin";
+var uri = "mongodb://aayush24:<password>@meanbeta-shard-00-00-sshqm.mongodb.net:27017,meanbeta-shard-00-01-sshqm.mongodb.net:27017,meanbeta-shard-00-02-sshqm.mongodb.net:27017/users?ssl=true&replicaSet=MEANbeta-shard-0&authSource=admin";
 
 router.get('/', (req, res)=> {
   res.send("Api Works");
